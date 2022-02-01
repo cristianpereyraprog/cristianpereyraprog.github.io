9f73e53add2852411792f996e7759b359ca1782a
cristianpereyraprog <77029414+cristianpereyraprog@users.noreply.github.com>
2022-02-01 19:21:20 -0300

    add img

--- backendEC/public/js/controladores/productos.js
+++ backendEC/public/js/controladores/productos.js
@@ -28,7 +28,7 @@ class ProductoController extends ProductoModel {
         let productoActualizado = await productoService.actualizarProductoService(id,producto)
         console.log(productoActualizado)
 
-        let index = this.productos.findIndex(producto => producto.id == productoActualizado.id)
+        let index = this.productos.findIndex(producto => producto._id == productoActualizado._id)
         this.productos.splice(index,1,productoActualizado)
 
         renderTablaAlta(null, this.productos)
